1ff3595add45e691df8a3c00bedfb62e16afa87b
Gaganc-dot <gaganchoudhary213@gmail.com>
2025-05-30 23:12:49 +0530

    Create Assignment10_1.py

---
+++ Assignment10_1.py
@@ -0,0 +1,12 @@
+Power = lambda X : X**2
+
+def main():
+    print("Enter your number :")
+    n = int(input())
+    
+    ans =Power(n)
+    print("Power is : ",ans)
+    
+if __name__ == "__main__":
+    main()
+        
