b80e5d3ce8ac9d310af5ab47a5a5bd973b91fd9c
Gaganc-dot <gaganchoudhary213@gmail.com>
2025-05-30 23:13:13 +0530

    Create Assignment10_2.py

---
+++ Assignment10_2.py
@@ -0,0 +1,14 @@
+Multiplication = lambda X, Y : X* Y
+
+def main():
+    print("Enter your number :")
+    A = int(input())
+    
+    print("Enter your number :")
+    B = int(input())
+    ans =Multiplication(A, B)
+    print("multiplication is : ",ans)
+    
+if __name__ == "__main__":
+    main()
+        
